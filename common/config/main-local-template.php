489b8d259f4c083758eea79a0ad76f21963b576a
Shinidoki <kassk_fl@hotmail.de>
2016-11-25 10:25:03 +0100

    Config Template added

---
+++ common/config/main-local-template.php
@@ -0,0 +1,33 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=host;dbname=dbname',
+            'username' => 'user',
+            'password' => 'pw',
+            'charset' => 'utf8',
+            'tablePrefix' => 'cah_'
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            // Disable index.php
+            'showScriptName' => false,
+            // Disable r= routes
+            'enablePrettyUrl' => true,
+            'rules' => array(
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+            ),
+        ],
+    ],
+];
